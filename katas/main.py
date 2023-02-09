f9403013ec51c3ab5713c05428e7bd61a66d2b03
aleks <atkbvs@gmail.com>
2023-02-09 16:06:20 +0300

    Even or Odd.

--- katas/main.py
+++ katas/main.py
@@ -9,17 +9,12 @@
 # Write a function calculating distance between Point a and Point b.
 # Tests compare expected result and actual answer with tolerance of 1e-6.
 
-import math
+def even_or_odd(number):
+    if number %2 == 0:
+        return "Even"
+    else:
+        return "Odd"
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-def distance_between_points(a, b):
-    return math.sqrt(((b.x - a.x) * (b.x - a.x)) + ((b.y - a.y) * (b.y - a.y)))
-
-res = distance_between_points(Point(1, 6), Point(4, 2))
-print(res)
+print(even_or_odd(4))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
